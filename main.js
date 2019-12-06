1a9dc3d1099cee1fc03e3fe9c4c4a92c873159a4
npezzotti <npezzotti@hotmail.com>
2019-12-06 13:58:37 -0500

    more functionality

--- main.js
+++ main.js
@@ -8,37 +8,67 @@ keys.addEventListener("click", e => {
         const key = e.target
         const action = key.dataset.action
         const keyText = key.textContent
-        const previousKeyType = calculator.dataset.previousKeyType
+        const prevKey = calculator.dataset.prevKey
         Array.from(key.parentNode.children)
             .forEach(key => key.classList.remove("is-clicked"))
         if (!action) {
-            if (display.textContent === "0" || previousKeyType === "operator") {
+            if (display.textContent === "0" || prevKey === "operator" || prevKey === "calculate") {
                 display.textContent = keyText
             } else {
                 display.textContent += keyText
             }
-            calculator.dataset.previousKeyType = "number"
-        } else if (action === "multiply" || action === "divide" || action === "subtract" || action === "add") {
-            key.classList.add("is-clicked")
-            calculator.dataset.firstNum = display.textContent
-            calculator.dataset.operator = action
-            calculator.dataset.previousKeyType = "operator"
-        } else if (action === "calculate") {
-            const secondNum = display.textContent
-            const firstNum = calculator.dataset.firstNum
+            calculator.dataset.prevKey = "number"
+        } if (action === "multiply" || action === "divide" || action === "subtract" || action === "add") {
+            let firstNum = calculator.dataset.firstNum
             const operator = calculator.dataset.operator
-            display.textContent = calculate(firstNum, operator, secondNum)
-            calculator.dataset.previousKeyType = "calculate"
-        } else if (action === "decimal") {
+            const secondNum = display.textContent
+            if (firstNum && operator && prevKey !== "operator" && prevKey !== "calculate") {
+                const result = calculate(firstNum, operator, secondNum)
+                display.textContent = result
+                calculator.dataset.firstNum = result 
+            } else {
+                calculator.dataset.firstNum = display.textContent
+            }
+            key.classList.add("is-clicked")
+            calculator.dataset.operator = action
+            calculator.dataset.prevKey = "operator"
+        } if (action === "calculate") {
+            let firstNum = calculator.dataset.firstNum
+            const operator = calculator.dataset.operator
+            let secondNum = display.textContent
+            if (firstNum) {
+                if (prevKey === "calculate") {
+                    firstNum = display.textContent
+                    secondNum = calculator.dataset.modifier
+                    display.textContent = calculate(firstNum, operator, secondNum)        
+                }
+                display.textContent = calculate(firstNum, operator, secondNum)
+            }
+            calculator.dataset.modifier = secondNum
+            calculator.dataset.prevKey = "calculate"
+        } if (action === "decimal") {
             if (!display.textContent.includes(".")) {
                 display.textContent += ".";
-            } else if (previousKeyType === "operator") {
+            } 
+            if (prevKey === "operator" || prevKey === "calculate") {
                 display.textContent = "0."
             }
-            calculator.dataset.previousKeyType = "decimal"
-        } else if (action === "clear") {
+            calculator.dataset.prevKey = "decimal"
+        } if (action === "clear") {
+            if (key.textContent === "AC") {
+                calculator.dataset.firstNum = ""
+                calculator.dataset.modifier = ""
+                calculator.dataset.operator = ""
+                calculator.dataset.prevKey = ""
+            } else {
+                display.textContent = "0"
+                key.textContent = "AC"
+            }
             display.textContent = "0"
-            calculator.dataset.previousKeyType = "clear"
+            calculator.dataset.prevKey = "clear"
+        } if (action !== "clear") {
+            const clear = calculator.querySelector('[data-action=clear]')
+            clear.textContent = 'CE'
         }
     } else {
         console.log("Not a valid key!")
